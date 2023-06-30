052a58296f43ebfbfcd64029165fc82a1b1e18f8
aritroCoder <ogcodinggod@gmail.com>
2023-06-30 11:22:40 +0530

    fixed add credentials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,97 +1,117 @@
-"use client"
-import Web3 from 'web3'
+"use client";
+import Web3 from "web3";
 import { ethers } from "ethers";
-import CryptoJS from 'crypto-js'
-import { useEffect, useState } from 'react'
-import contractABI from '../constants/abi.json'
-
-interface Credentials{
-  website: string,
-  username: string,
-  password: string,
-  en_password: string,
-  en_username: string,
-}
+import CryptoJS from "crypto-js";
+import { useEffect, useState } from "react";
+import contractABI from "../constants/abi.json";
 
 export default function Home() {
-  const [domReady, setDomReady] = useState(false)
-  const [connected, setConnected] = useState(false)
+  const [domReady, setDomReady] = useState(false);
+  const [connected, setConnected] = useState(false);
+  const [signer, setSigner] = useState<ethers.JsonRpcSigner | null>(null);
   const [credential, setCredential] = useState({
     website: "",
     username: "",
     password: "",
-  })
+  });
   const [en_credential, setEnCredential] = useState({
     en_username: "",
     en_password: "",
-  })
-  const [passPhrase, setPassphrase] = useState("")
-  const [encryptedMessage, setEncryptedMessage] = useState("")
-  const [signature, setSignature] = useState("")
-  const [accounts, setAccounts] = useState([])
-  const contractAddress = "0xc4C62aAE5eA3dE459Dd83A2Cc6BFdC33D53eFD41";
-  let contract: ethers.Contract | null = null;
+  });
+  const [passPhrase, setPassphrase] = useState("");
+  const [encryptedMessage, setEncryptedMessage] = useState("");
+  const [signature, setSignature] = useState("");
+  const [accounts, setAccounts] = useState([]);
+  const [contract, setContract] = useState<ethers.Contract | null>(null);
+  const contractAddress = "0xA2cF1A444f4aB42b02b1053D7d87540f3Bced6ef";
 
   useEffect(() => {
-    setDomReady(true)
-  }, [])
+    setDomReady(true);
+  }, []);
 
   const connect = async () => {
-    if(window.ethereum){
+    if (window.ethereum) {
       const provider = new ethers.BrowserProvider(window.ethereum);
       await provider.send("eth_requestAccounts", []);
       const signer = await provider.getSigner();
-      contract = new ethers.Contract(contractAddress, contractABI, signer);
-      console.log({signer, contract})
+      let contract = new ethers.Contract(contractAddress, contractABI, signer);
+      setContract(contract);
+      console.log({ signer, contract });
+      setSigner(signer);
+      setConnected(true);
+    } else {
+      alert("No wallet found");
     }
-    else{
-      alert('No wallet found')
-    }
-  }
+  };
 
   const encryptMessage = async (credential: {
-    website: string,
-    username: string,
-    password: string,
+    website: string;
+    username: string;
+    password: string;
   }) => {
-    let en_username = CryptoJS.AES.encrypt(credential.username, passPhrase).toString()
-    let en_password = CryptoJS.AES.encrypt(credential.password, passPhrase).toString()
-    setEnCredential({en_username, en_password})
-  }
+    let en_username = CryptoJS.AES.encrypt(
+      credential.username,
+      passPhrase
+    ).toString();
+    let en_password = CryptoJS.AES.encrypt(
+      credential.password,
+      passPhrase
+    ).toString();
+    setEnCredential({ en_username, en_password });
+  };
 
   const addCreds = async (e: any) => {
-    console.log("Contract call")
     e.preventDefault();
     if (contract) {
+      console.log("Contract call");
       try {
         const tx = await contract.addCredentials(
           credential.website,
           en_credential.en_username,
           en_credential.en_password
         );
+        contract.on("added_temp_credentials", async (merkleRoot: string) => {
+          console.log({ merkleRoot });
+          let signature = await signer?.signMessage(
+            ethers.getBytes(merkleRoot)
+            // merkleRoot
+          );
+          console.log({ signature });
+          if(contract && signature){
+            console.log("Sign merkleroot")
+            try {
+              let tx_approve = await contract.approveCredentials(
+                signature
+              );
+              console.log(tx_approve)
+            } catch (error) {
+              console.log(error)
+            }
+          }
+        });
         await tx.wait();
         console.log({ tx });
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     }
   };
 
   const signMessage = async (encryptedMessage: string) => {
-    if(accounts.length === 0){
-      alert('Please connect wallet first')
-      return
+    if (accounts.length === 0) {
+      alert("Please connect wallet first");
+      return;
     }
-    if(window.ethereum){
-      window.web3 = new Web3(window.ethereum)
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
       const signature: string = await window.web3.eth.personal.sign(
         encryptedMessage,
         accounts[0]
       );
-      console.log({signature})
-      setSignature(signature)
+      console.log({ signature });
+      setSignature(signature);
     }
-  }
+  };
 
   return (
     <div>
@@ -101,7 +121,6 @@ export default function Home() {
           <button
             onClick={() => connect()}
             className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4`}
-            disabled={connected}
           >
             {!connected ? "Connect Wallet" : "Wallet Connected"}
           </button>
