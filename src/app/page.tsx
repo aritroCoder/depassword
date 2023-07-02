b49f5b81141dfb72ebfce5a62a4626d3093bef9f
Nidhi-Sharma9419 <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2023-07-02 10:16:04 +0530

    Added decryptMessage Function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,21 @@ export default function Home() {
 
   useEffect(() => {
     setDomReady(true);
+
   }, []);
+
+  const decryptMessage = () => {
+    if (passPhrase && en_credential.en_username && en_credential.en_password) {
+      const decryptedUsername = CryptoJS.AES.decrypt(en_credential.en_username, passPhrase).toString(
+        CryptoJS.enc.Utf8
+      );
+      const decryptedPassword = CryptoJS.AES.decrypt(en_credential.en_password, passPhrase).toString(
+        CryptoJS.enc.Utf8
+      );
+      console.log('Decrypted Username:', decryptedUsername);
+      console.log('Decrypted Password:', decryptedPassword);
+    }
+  };
 
   const connect = async () => {
     if (window.ethereum) {
@@ -202,6 +216,14 @@ export default function Home() {
         >
           Add Credential
         </button>
+        {/* decrypt message */}
+        <button
+          className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4`}
+          onClick={decryptMessage}
+        >
+          Decrypt Message
+        </button>
+
       </div>
     </div>
   );
